6b3538e2ff3eb38b6a78775e36a20691b30caffa
1337-hacks <elijahnucum.exe@gmail.com>
2022-12-08 01:19:37 +1300

    c003 better homepage layout

--- src/App.js
+++ src/App.js
@@ -8,28 +8,32 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { config } from 'react-spring'
 
 function App() {
-  const alignCenter = { display: 'flex', alignItems: 'center' }
   return (
     <>
-      <Container fluid>
+      <Container fluid style={{position: "relative", height: "200vh"}}>
         <Row className='time-date'>
           <Col className='time'><p>5:12pm</p></Col>
           <Col className='date'><p>24th December 2022</p></Col>
         </Row>
-        <Row style={{height: "100vh", position: "relative"}}>
-          <Col>
-            <div className='landing-greeting' sticky={{start: 0, end: 1}}>
-              <h1>Good xxx,<br/>I'm Elijah.</h1>
+        <Row>
+          <Col style={{position: "relative", height: "200vh"}}>
+            <div className='landing-greeting'>
+              <h2>Good xxx,<br/>I'm Elijah.</h2>
             </div>
           </Col>
+          <Col style={{position: "relative", paddingTop: "100vh"}}>
+            <div className='page-links'><a href="#about"><h1><span>About</span></h1></a></div>
+            <div className='page-links'><a href="#projects"><h1><span>Projects</span></h1></a></div>
+            <div className='page-links'><a href="#linkedin"><h1><span>LinkedIn</span></h1></a></div>
+            <div className='page-links'><a href="#github"><h1><span>GitHub</span></h1></a></div>
+            <div className='page-links'><a href="#contact"><h1><span>Contact</span></h1></a></div>            
+          </Col>
         </Row>
-        <Row style={{height: "100vh", position: "relative"}}>
-          <Col>
-            <div className='page-links'><h1>About</h1></div>
-            <div className='page-links'><h1>Projects</h1></div>
-            <div className='page-links'><h1>LinkedIn</h1></div>
-            <div className='page-links'><h1>GitHub</h1></div>
-            <div className='page-links'><h1>Contact</h1></div>            
+      </Container>
+      <Container fluid style={{position: "relative", height: "100vh"}}>
+        <Row>
+          <Col style={{position: "absolute", bottom: "0%", textAlign: "center"}}>
+            <p>Made by Elijah Nucum. All rights reserved 2022</p>
           </Col>
         </Row>
       </Container>
