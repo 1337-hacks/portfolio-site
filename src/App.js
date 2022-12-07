5ed565b02250a8775750bcef08557515997d2b28
1337-hacks <elijahnucum.exe@gmail.com>
2022-12-07 23:58:06 +1300

    c002 homepage progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { config } from 'react-spring'
 
 function App() {
+  const alignCenter = { display: 'flex', alignItems: 'center' }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container fluid>
+        <Row className='time-date'>
+          <Col className='time'><p>5:12pm</p></Col>
+          <Col className='date'><p>24th December 2022</p></Col>
+        </Row>
+        <Row style={{height: "100vh", position: "relative"}}>
+          <Col>
+            <div className='landing-greeting' sticky={{start: 0, end: 1}}>
+              <h1>Good xxx,<br/>I'm Elijah.</h1>
+            </div>
+          </Col>
+        </Row>
+        <Row style={{height: "100vh", position: "relative"}}>
+          <Col>
+            <div className='page-links'><h1>About</h1></div>
+            <div className='page-links'><h1>Projects</h1></div>
+            <div className='page-links'><h1>LinkedIn</h1></div>
+            <div className='page-links'><h1>GitHub</h1></div>
+            <div className='page-links'><h1>Contact</h1></div>            
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
