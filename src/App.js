2b3a7cf5cb55aeae856b85f9ee0e428c0edc515e
1337-hacks <elijahnucum.exe@gmail.com>
2022-12-08 13:31:03 +1300

    c005 added mobile-friendly layout

--- src/App.js
+++ src/App.js
@@ -10,29 +10,27 @@ import { config } from 'react-spring'
 function App() {
   return (
     <>
-      <Container fluid style={{position: "relative", height: "200vh"}}>
+      <Container fluid style={{height: "200vh"}}>
         <Row className='time-date'>
           <Col className='time'><p>5:12pm</p></Col>
           <Col className='date'><p>24th December 2022</p></Col>
         </Row>
         <Row>
-          <Col style={{position: "relative", height: "200vh"}}>
+          <Col className='landing-col'>
             <div className='landing-greeting'>
               <h2>Good xxx,<br/>I'm Elijah.</h2>
             </div>
           </Col>
-          <Col style={{position: "relative", paddingTop: "100vh"}}>
-            <div className='page-links'><h1><a href="#about">About</a></h1></div>
-            <div className='page-links'><h1><a href="#projects">Projects</a></h1></div>
-            <div className='page-links'><h1><a href="#linkedin">LinkedIn</a></h1></div>
-            <div className='page-links'><h1><a href="#github">GitHub</a></h1></div>
-            <div className='page-links'><h1><a href="#contact">Contact</a></h1></div>
+          <Col className='links-col'>
+            <div className='links'><h1><a href="#about">About</a></h1></div>
+            <div className='links'><h1><a href="#projects">Projects</a></h1></div>
+            <div className='links'><h1><a href="#linkedin">LinkedIn</a></h1></div>
+            <div className='links'><h1><a href="#github">GitHub</a></h1></div>
+            <div className='links'><h1><a href="#contact">Contact</a></h1></div>
           </Col>
         </Row>
-      </Container>
-      <Container fluid style={{position: "relative", height: "100vh"}}>
         <Row>
-          <Col style={{position: "absolute", bottom: "0%", textAlign: "center"}}>
+          <Col className='footer-col'>
             <p>Made by Elijah Nucum. All rights reserved 2022</p>
           </Col>
         </Row>
