644baac8aed10b0ce2fa1d96430ebfd595a76a3f
1337-hacks <elijahnucum.exe@gmail.com>
2023-01-24 18:05:39 +1300

    c015 removed react-bootstrap

--- src/content/About.js
+++ src/content/About.js
@@ -1,31 +1,27 @@
 import '../App.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Stack from 'react-bootstrap/Stack'
 
 function About(props) {
 
     return (
-        <Container className='about-page'>
-            <Row>
-                <Col md={3} className='about-area'>
+        <div className='about-page'>
+            <div>
+                <div className='about-area'>
                     <div className='about-title'>
                         <h2>About</h2>
                     </div>
                     <div className='back-button'>
                         <a href='#home' onClick={props.back}><h2>Back</h2></a>
                     </div>
-                </Col>
-                <Col md={9} className='about-description'>
+                </div>
+                <div className='about-description'>
                     <h3>
                         I am a recent graduate of Auckland University of Technology. <br/><br/>
                         I love to use my creativity to make innovative things and create interesting experiences. <br/><br/>
                         I also love going to the gym and listening to music, mainly rap and R&B.
                     </h3>
-                </Col>
-            </Row>
-        </Container>
+                </div>
+            </div>
+        </div>
     );
 }
 
