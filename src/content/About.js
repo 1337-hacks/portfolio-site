712ac757f8f13475cbe37f45fc8f82bec3fc259f
1337-hacks <elijahnucum.exe@gmail.com>
2023-02-02 17:29:02 +1300

    c022 completed about page

--- src/content/About.js
+++ src/content/About.js
@@ -16,13 +16,22 @@ function About() {
                 </div>    
             </div>
             <div className='about-description'>
-                <h4>
+                <h4 className='desc'>
                     I am a recent graduate of Auckland University of Technology where I finished a Bachelors degree in Computer and Information Science.
                     <br/><br/>
                     After finishing university, I have continued to explore the world of web development, where I’ve found a deep interest in frontend web development and web design.
                     <br/><br/>
                     In my own free time I love going to the gym, listening to music and working on my own personal projects.
                 </h4>
+            </div>
+            <div className='about-technologies'>
+                <h3>Skills and Technologies</h3>
+                <div className='about-icons'>
+                    <ion-icon name="logo-react"></ion-icon>
+                    <ion-icon name="logo-javascript"></ion-icon>
+                    <ion-icon name="logo-figma"></ion-icon>
+                    <ion-icon src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Three.js_Icon.svg"></ion-icon>
+                </div>
             </div>
         </div>
     );
